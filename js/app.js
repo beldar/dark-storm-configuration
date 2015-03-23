6a544a7acf4a7b40322048a1781c21c735161af7
Beldar <beldar.cat@gmail.com>
2015-03-23 21:10:16 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+angular.module('ionicApp', ['ionic'])
+
+.controller('MainCtrl', ['$scope', '$window', function($scope, $window) {
+
+    $scope.tempUnits = [
+        { text: 'Celcius', value: 'C' },
+        { text: 'Farenheit', value: 'F' },
+        { text: 'Kelvin', value: 'K' }
+    ];
+    
+    $scope.data = {
+        tempUnit: 'C',
+        inverted: false,
+        location: '',
+        gps: true,
+        refresh: 30
+    };
+    
+    $scope.saveData = function() {
+        console.log('Send Data', this.data); 
+        $window.location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(this.data));
+    };
+    
+    $scope.cancel = function() {
+        console.log('Cancel'); 
+        $window.location.href = 'pebblejs://close';
+    };
+
+}]);
